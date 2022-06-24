e0c3e4cfad869f658d8252fb3770909eb01a686b
jmcushnie <94955791+jmcushnie@users.noreply.github.com>
2022-06-24 21:50:10 +1200

    Update script.js
    function that loads questions and answers

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const quizContainer = document.getElementById("quiz")
 const submitBtn = document.getElementsByName("button")
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const questionEl = document.getElementById("question")
+
 
 // quiz questions
 const quizQuestions = [
@@ -48,4 +53,25 @@ const quizQuestions = [
         },
         correctAnswer: "a",
     },
-]
+];
+
+let currentQuestion = 0;
+
+loadQuiz();
+
+// load quiz questions and answers
+function loadQuiz() {
+    const currentQuiz = quizQuestions[currentQuestion];
+    
+    questionEl.innerHTML = currentQuiz.question;
+    a_text.innerHTML = currentQuiz.answers.a;
+    b_text.innerHTML = currentQuiz.answers.b;
+    c_text.innerHTML = currentQuiz.answers.c; 
+    
+}
+
+submitBtn.addEventListener("click", () => {
+    currentQuestion++;
+
+    loadQuiz();
+})
